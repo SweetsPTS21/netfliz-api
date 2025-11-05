d3dab185a961c75036a544f7ae18065036b1d751
sonpt96 <sonpt96@ghtk.co>
2025-11-05 15:45:32 +0700

    update cors config p2

---
+++ src/main/java/com/netfliz/netfliz/util/CommonProperties.java
@@ -0,0 +1,16 @@
+package com.netfliz.netfliz.util;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode
+@Configuration
+@ConfigurationProperties(prefix = "spring.cors")
+public class CommonProperties {
+    private Set<String> allowedOriginPatterns = Set.of("http://localhost:*,https://*.swpts.cloud,https://*.up.railway.app");
+}
