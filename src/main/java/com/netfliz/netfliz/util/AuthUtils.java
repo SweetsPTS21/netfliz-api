18544d7b0f328b6d2168aa6afd68556b8ff4976f
sonpt96 <sonpt96@ghtk.co>
2025-11-10 10:17:40 +0700

    update auth utils

--- src/main/java/com/netfliz/netfliz/util/AuthUtils.java
+++ src/main/java/com/netfliz/netfliz/util/AuthUtils.java
@@ -23,17 +23,17 @@ public class AuthUtils {
      */
     public UserEntity getCurrentUser() {
         // 1) thử lấy từ security context trước
-        String usernameOrEmail = getPrincipalFromSecurityContext();
-        if (usernameOrEmail == null) {
+        String username = getPrincipalFromSecurityContext();
+        if (username == null) {
             // 2) fallback: lấy từ JWT trong request
-            usernameOrEmail = getUsernameFromJwtInRequest();
+            username = getUsernameFromJwtInRequest();
         }
 
-        if (usernameOrEmail == null) {
+        if (username == null) {
             return null; // hoặc throw
         }
 
-        return userRepository.findByEmail(usernameOrEmail).orElse(null);
+        return userRepository.findByUsername(username).orElse(null);
     }
 
     /**
