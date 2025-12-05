811bfa43450a1df3202cbdcce77f4a20954b5de6
swpts <pts.28032001@gmail.com>
2025-12-05 20:03:28 +0700

    update config

---
+++ src/main/java/com/netfliz/netfliz/constant/CommonConfig.java
@@ -0,0 +1,25 @@
+package com.netfliz.netfliz.constant;
+
+public class CommonConfig {
+    public static final String[] WHITE_LIST_URL = {
+            "/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/graphiql/**",
+            "/graphql",
+            "/api/v1/file/presign-url",
+            "/api/v1/auth/register",
+            "/api/v1/auth/authenticate",
+            "/api/v1/auth/refresh-token",
+            "/api/v1/file/presign-url"
+    };
+}
