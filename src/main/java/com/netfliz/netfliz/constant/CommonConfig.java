d02ab9980933662a5ff18330d5a3b29a0c267e2b
sonpt96 <sonpt96@ghtk.co>
2025-12-13 11:49:08 +0700

    update url

--- src/main/java/com/netfliz/netfliz/constant/CommonConfig.java
+++ src/main/java/com/netfliz/netfliz/constant/CommonConfig.java
@@ -20,6 +20,7 @@ public class CommonConfig {
             "/api/v1/auth/authenticate",
             "/api/v1/auth/refresh-token",
             "/api/v1/file/presign-url",
-            "/api/v1/utils/mermaid-file/**"
+            "/api/v1/utils/mermaid-file/**",
+            "/api/v1/utils/upload-file/**"
     };
 }
